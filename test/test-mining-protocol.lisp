0bde978e15449eff368f46f2c740d25aa5fa7637
Park Ian Co <opensource@parkian.co>
2026-03-15 00:10:10 -0400

    Relicense from BSD-3-Clause to Apache-2.0

--- test/test-mining-protocol.lisp
+++ test/test-mining-protocol.lisp
@@ -1,10 +1,10 @@
 ;; Copyright (c) 2024-2026 Parkian Company LLC. All rights reserved.
-;; SPDX-License-Identifier: BSD-3-Clause
+;; SPDX-License-Identifier: Apache-2.0
 
 ;;;; test-mining-protocol.lisp - Unit tests for mining-protocol
 ;;;;
 ;;;; Copyright (c) 2024-2026 Parkian Company LLC. All rights reserved.
-;;;; SPDX-License-Identifier: BSD-3-Clause
+;;;; SPDX-License-Identifier: Apache-2.0
 
 (defpackage #:cl-mining-protocol.test
   (:use #:cl)
